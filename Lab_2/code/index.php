4a42170e419907b1e0991803f1d5a6a4ba9bcc58
choujii <113545983+choujii@users.noreply.github.com>
2024-03-10 00:40:54 +0200

    Declare an integer variable, output its value. Declare a floating-point variable, output its value. Create variables for past and current month expenses, output their difference.

--- Lab_2/code/index.php
+++ Lab_2/code/index.php
@@ -8,3 +8,28 @@ $order .= " with barbecue sauce";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+// Declaring a variable with an integer value
+$integer_number = 5;
+// Выводим значение переменной на терминал
+echo $integer_number;
+
+// Printing a new line
+echo "\n";
+
+// Declaring a floating-point variable
+$float_number = 3.14;
+// Output the value of the new variable to the terminal
+echo $float_number;
+
+// We print the number 12
+echo 6 + 6;
+
+// How much more did I spend last month than this month?
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+// We display the difference between expenses last month and this month
+echo "\nDifference in expenses: " . ($last_month - $this_month);
+
+
